c77402871dd110dc5314ee4a2dcdc0e1b6163d14
Kosmas Chatzimichalis <kosmas@mach7x.com>
2016-03-16 22:25:06 +0100

    reimplement pagination with angular

--- app/assets/javascripts/customers_app.js
+++ app/assets/javascripts/customers_app.js
@@ -3,15 +3,28 @@ var app = angular.module('customers', []);
 app.controller("CustomerSearchController", [
           "$scope","$http",
   function($scope, $http) {
+    var page = 0;
     $scope.search = function(searchTerm) {
       $http.get("/customers.json",
-                { "params": { "keywords": searchTerm } }
+                { "params": { "keywords": searchTerm, "page": page  } }
       ).then(function(response) {
           $scope.customers = response.data;
       },function(response) {
             alert("There was a problem: " + response.status);
         }
       );
+    }
+
+    $scope.previousPage = function() {
+      if (page > 0) {
+        page = page - 1;
+        $scope.search($scope.keywords);
+      }
+    }
+
+    $scope.nextPage = function() {
+      page = page + 1;
+      $scope.search($scope.keywords);
     }
   }
 ]);
