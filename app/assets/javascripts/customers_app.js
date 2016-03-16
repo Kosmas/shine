a7a310873d3492185025792f50e33381054a693f
Kosmas Chatzimichalis <kosmas@mach7x.com>
2016-03-16 22:29:23 +0100

    add minimum length for search

--- app/assets/javascripts/customers_app.js
+++ app/assets/javascripts/customers_app.js
@@ -5,6 +5,9 @@ app.controller("CustomerSearchController", [
   function($scope, $http) {
     var page = 0;
     $scope.search = function(searchTerm) {
+      if (searchTerm.length < 3) {
+        return;
+      }
       $http.get("/customers.json",
                 { "params": { "keywords": searchTerm, "page": page  } }
       ).then(function(response) {
